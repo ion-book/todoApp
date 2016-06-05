ddebef388b18c205284cfeb48e812a5df88b1609
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-06-05 18:49:15 -0400

    Agregar metodo config

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,10 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
+.config(function($stateProvider, $urlRouterProvider){
+  //Definir las rutas
+})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
