17c35f9ac8d4f179a02afd3206c3745bcfd21510
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-06-06 00:09:57 -0400

    Agregar objeto de fechas agrupadas

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -19,7 +19,26 @@ angular.module('starter.controllers', [])
   ]
 })
 .controller('AllCtrl', function(){
-  console.log('AllCtrl');
+  var obj =
+  {
+    dateGroup: new Date(2015, 6, 5),
+    tasks: [
+      {
+        id:1,
+        title: 'Hacer las compras',
+        description: 'Dulces, leche, chocolates y gomitas',
+        date: new Date(),
+        done: false
+      },
+      {
+        id:2,
+        title: 'Sacar la mascota',
+        description: 'Dar un paseo en el parque más cercano',
+        date: new Date(),
+        done: true
+      },
+    ]
+  }
 })
 .controller('DoneCtrl', function(){
   console.log('DoneCtrl');
