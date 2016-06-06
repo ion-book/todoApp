50d4a0a50c4ccd5a9be42a4a0b7fede7ca1d7324
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-06-05 23:40:33 -0400

    Crear un array de tareas

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,13 +1,22 @@
 angular.module('starter.controllers', [])
 
 .controller('TodayCtrl', function( $scope ){
-  $scope.task = {
-    id:1,
-    title: 'Hacer las compras',
-    description: 'Dulces, leche, chocolates y gomitas',
-    date: new Date(),
-    done: false
-  }
+  $scope.tasks = [
+    {
+      id:1,
+      title: 'Hacer las compras',
+      description: 'Dulces, leche, chocolates y gomitas',
+      date: new Date(),
+      done: false
+    },
+    {
+      id:2,
+      title: 'Sacar la mascota',
+      description: 'Dar un paseo en el parque más cercano',
+      date: new Date(),
+      done: true
+    },
+  ]
 })
 .controller('AllCtrl', function(){
   console.log('AllCtrl');
