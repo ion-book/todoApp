e5e5b50c266d273e4d33ac75b3aeeea28f174f9e
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-06-06 00:25:56 -0400

    Agregar fechas en el controlador DoneCtrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -62,7 +62,48 @@ angular.module('starter.controllers', [])
   ];
   
 })
-.controller('DoneCtrl', function(){
-  console.log('DoneCtrl');
+.controller('DoneCtrl', function( $scope ){
+  
+  $scope.dates = [
+    {
+      dateGroup: new Date(2015, 6, 5),
+      tasks: [
+        {
+          id:1,
+          title: 'Hacer las compras',
+          description: 'Dulces, leche, chocolates y gomitas',
+          date: new Date(),
+          done: false
+        },
+        {
+          id:2,
+          title: 'Sacar la mascota',
+          description: 'Dar un paseo en el parque más cercano',
+          date: new Date(),
+          done: true
+        },
+      ]
+    },
+    {
+      dateGroup: new Date(2015, 6, 6),
+      tasks: [
+        {
+          id:3,
+          title: 'Pagar las facturas',
+          description: 'Ir al banco y pagar todas las facturas',
+          date: new Date(),
+          done: false
+        },
+        {
+          id:4,
+          title: 'Ver una buena pelicula',
+          description: 'Como el "El club de la pelea"',
+          date: new Date(),
+          done: true
+        },
+      ]
+    },
+  ];
+  
 })
 
