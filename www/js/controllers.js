e30d8827b0ed0d4adc557dc3546cd91deda92dda
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-06-05 22:49:16 -0400

    agregar task

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,14 @@
 angular.module('starter.controllers', [])
 
 .controller('TodayCtrl', function(){
-  console.log('TodayCtrl');
+  $scope.task =
+  {
+    id:1,
+    title: 'Hacer las compras',
+    description: 'Dulces, leche, chocolates y gomitas',
+    date: new Date(),
+    done: false
+  }
 })
 .controller('AllCtrl', function(){
   console.log('AllCtrl');
